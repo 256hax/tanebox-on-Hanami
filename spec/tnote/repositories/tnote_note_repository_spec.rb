ed96d709eb29bba3a89e5e51ec94d57bd9bf02d8
256hax <256hax+git@gmail.com>
2018-03-04 22:21:06 +0900

    UPDATE: split factories data

--- spec/tnote/repositories/tnote_note_repository_spec.rb
+++ spec/tnote/repositories/tnote_note_repository_spec.rb
@@ -1,12 +1,15 @@
 require_relative '../../spec_helper'
 require_relative '../../factories/tnote_note_factory'
+require_relative '../../factories/tnote_stream_factory'
 
 describe TnoteNoteRepository do
   test_data = TnoteNoteFactory.new.create # call factories/tnote_note_factory.rb
+  note_id = test_data[:note].id
+  test_data.merge!(TnoteStreamFactory.new.create(note_id))
 
   it 'TnoteNote with TnoteStream associations are successful' do
-    @note_with_all_associations = Tnote::IndexNoteWithAllAssociations.new.call(test_data[:note].id)
+    @note_with_all_associations = Tnote::IndexNoteWithAllAssociations.new.call(note_id) # call Interactor
     stream_content = @note_with_all_associations.list.tnote_streams[0].content
-    stream_content.must_equal 'do it'
+    stream_content.must_equal 'no issues'
   end
 end
