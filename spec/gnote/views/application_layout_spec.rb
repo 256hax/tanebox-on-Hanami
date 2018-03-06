6eeb06a54673a25c4f6c21aee255bde4bc9cad9a
256hax <256hax+git@gmail.com>
2018-03-07 00:51:37 +0900

    ADD: spec

--- spec/gnote/views/application_layout_spec.rb
+++ spec/gnote/views/application_layout_spec.rb
@@ -1,14 +1,15 @@
 require "spec_helper"
 
 describe Gnote::Views::ApplicationLayout do
-  meta_title = 'Good Note for me'
-
   let(:layout)   { Gnote::Views::ApplicationLayout.new(template, {}) }
   let(:rendered) { layout.render }
   let(:template) { Hanami::View::Template.new('apps/gnote/templates/application.html.erb') }
 
+  it 'exposes #meta_title' do
+    layout.meta_title.must_equal 'ほめノート'
+  end
 
   it 'contains application name' do
-    rendered.must_include('Gnote')
+    rendered.must_include('ほめノート')
   end
 end
--- spec/tnote/repositories/tnote_note_repository_spec.rb
+++ spec/tnote/repositories/tnote_note_repository_spec.rb
@@ -3,9 +3,11 @@ require_relative '../../factories/tnote_note_factory'
 require_relative '../../factories/tnote_stream_factory'
 
 describe TnoteNoteRepository do
-  @test_data = TnoteNoteFactory.new.create # call factories/tnote_note_factory.rb
-  @note_id = @test_data[:note].id
-  @test_data.merge!(TnoteStreamFactory.new.create(@note_id))
+  before do
+    @test_data = TnoteNoteFactory.new.create # call factories/tnote_note_factory.rb
+    @note_id = @test_data[:note].id
+    @test_data.merge!(TnoteStreamFactory.new.create(@note_id))
+  end
 
   it 'TnoteNote with TnoteStream associations are successful' do
     note_with_all_associations = Tnote::IndexNoteWithAllAssociations.new.call(@note_id) # call Interactor
